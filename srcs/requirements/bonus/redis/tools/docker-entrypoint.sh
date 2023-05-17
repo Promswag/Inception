deaf0c2f94abc59629d9fb465242211f20378ad9
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2023-05-17 14:48:11 +0200

    redis update

--- srcs/requirements/bonus/redis/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/redis/tools/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = '_start' ]; then
+	sed -i "s/REDIS_PASS/$REDIS_PASS/" /etc/redis/redis.conf
 	redis-server
 fi
 
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-if [ "$1" = 'wordpress' ]; then
+if [ "$1" = '_start' ]; then
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbcharset="utf8" --allow-root
 	wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --skip-email --allow-root
 	wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASS --allow-root
 	wp theme install inspiro --activate --allow-root
+
+	wp config set WP_REDIS_CLIENT phpredis
+	wp config set WP_REDIS_SCHEME tcp
+	wp config set WP_REDIS_HOST $REDIS_HOST
+	wp config set WP_REDIS_PORT $REDIS_PORT
+	wp config set WP_REDIS_PASSWORD $REDIS_PASS
+	wp plugin install redis-cache --activate
+	wp redis enable
 	exec php-fpm7.4 -F
 fi
 
